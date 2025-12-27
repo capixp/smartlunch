2cdc8ddd727b86ba482e9faf0723712977cd39c3
Capixp <warnerxp@gmail.com>
2025-12-27 11:02:56 -0600

    Order Screen

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import { useStore } from "@/src/store";
 import React from "react";
+import ProductDetails from "./ProductDetails";
 
 export default function OrderSummary() {
+  const order = useStore((state) => state.order);
   return (
     <aside className="md:h-screen md:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi pedido</h1>
+      {order.length === 0 ? (
+        <p className="text-center my-10">El carrito esta vacio</p>
+      ) : (
+        <div className="mt-5">
+          {order.map((item) => (
+            <ProductDetails key={item.id} item={item} />
+          ))}
+        </div>
+      )}
     </aside>
   );
 }
