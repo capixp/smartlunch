3457d3257ae39f5fdff44613dbfeb26f752c3204
Capixp <warnerxp@gmail.com>
2025-12-28 15:11:24 -0600

    fix deploy Logo

--- components/admin/AdminSidebar.tsx
+++ components/admin/AdminSidebar.tsx
@@ -1,10 +1,10 @@
 import Logo from "../ui/Logo";
 
-const adminNavigation = [
+/* const adminNavigation = [
   { url: "/admin/orders", text: "Ordenes", blank: false },
   { url: "/admin/products", text: "Productos", blank: false },
   { url: "/order/cafe", text: "Ver Quiosco", blank: true },
-];
+]; */
 
 export default function AdminSidebar() {
   return (
